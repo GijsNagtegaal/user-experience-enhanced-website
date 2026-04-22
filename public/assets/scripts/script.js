a4e9cc57d790c4c554d71913600cdc3033e26a34
Gijs <gijs.nagtegaal@hva.nl>
2026-04-22 22:40:49 +0200

    replaced images with partiasl and introduced new way to complete challanches [EPIC] Performance Fixes GijsNagtegaal/user-experience-enhanced-website#11

---
+++ public/assets/scripts/script.js
@@ -0,0 +1,21 @@
+const loaderBtn = document.querySelector('.loader');
+const allOptions = document.querySelectorAll('.options-container button');
+let timer;
+
+const triggerAnimation = (event) => {
+
+    const clickedBtn = event.currentTarget;
+
+    const targetUrl = clickedBtn.getAttribute('data-url');
+    loaderBtn.setAttribute('href', targetUrl);
+
+    loaderBtn.classList.remove('shownow', 'ready');
+    
+    void loaderBtn.offsetWidth; 
+
+    loaderBtn.classList.add('shownow');
+};
+
+allOptions.forEach(btn => {
+    btn.addEventListener('click', triggerAnimation);
+});
